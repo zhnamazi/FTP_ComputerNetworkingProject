1e920e69259d6084976a0bb40c6ad7ea907c027c
Zahra <znamazi81@gmail.com>
2024-02-02 22:40:25 +0330

    semantic finished

--- FTPclient.py
+++ FTPclient.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import socket
 import io
 import struct
@@ -11,6 +12,9 @@ controlSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 
 dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 
+Path("./Download Folder").mkdir(parents=True, exist_ok=True)
+
+
 def Help():
     list_of_commands = """\nHelp: Display list of commands
 List: Get the list of files along with the size of each file
@@ -21,11 +25,29 @@ Delete <file_path>: Delete a file from the given path
 Pwd: Display the current location on the server"""
     print(list_of_commands)
 
+
+def List():
+    controlSocket.sendall(str("List").encode('utf-8'))
+    controlSocket.recv(buffer)
+    controlSocket.sendall(str(".").encode('utf-8'))
+    num = struct.unpack("i" ,controlSocket.recv(buffer))[0]
+    for i in range(num):
+        name = controlSocket.recv(buffer).decode('utf-8')
+        controlSocket.sendall(str(".").encode('utf-8'))
+        size = struct.unpack("i", controlSocket.recv(buffer))[0]
+        controlSocket.sendall(str(".").encode('utf-8'))
+        # changed_size = size/1000
+        # if(size > 1000):
+        #     changed_size = changed_size / 1000
+        print(name, '   ', size)
+
+
 def Connect():
     try:
         controlSocket.connect((IP,controlPort))
     except:
         "unsuccessful connection"
+
 
 def Upload(file_path):
     controlSocket.sendall(str("Upload").encode('utf-8'))
@@ -45,24 +67,56 @@ def Upload(file_path):
     file.close()
     dataSocket.close()
 
-    
-    
 
-    
+def Download(fileName):
+    controlSocket.sendall(str("Download").encode('utf-8'))
+    controlSocket.recv(buffer)
+    dataSocket.connect((IP, dataPort))
+    controlSocket.sendall(fileName.encode('utf-8'))
+    fileSize = struct.unpack("i", controlSocket.recv(buffer))[0]
+    file_path = "./Download Folder/" + fileName
+    file = open(file_path, "wb")
+    comingSize = 0
+    while comingSize < fileSize:
+        chunk = dataSocket.recv(buffer)
+        file.write(chunk)
+        comingSize += buffer
+    file.close()
+    dataSocket.close()
 
 
+def Delete(fileName):
+    controlSocket.sendall(str("Delete").encode('utf-8'))
+    controlSocket.recv(buffer)
+    controlSocket.sendall(fileName.encode('utf-8'))
+       
+
+def Pwd():
+    controlSocket.sendall(str("Pwd").encode('utf-8'))
+    controlSocket.recv(buffer)
+    controlSocket.sendall(str(".").encode('utf-8'))
+    path = controlSocket.recv(buffer).decode('utf-8')
+    print(path)
 
 
 command = input("Client is running. Enter your command (type \"Help\" to see commands)\n")
 while command!="q":
-    if(command.startswith("Help")):
+    if command.startswith("Help"):
         Help()
-    elif(command.startswith("Connect")):
+    elif command.startswith("List"):
+        List()
+    elif command.startswith("Connect"):
         Connect()
-    elif(command.startswith("Upload ")):
+    elif command.startswith("Upload "):
         Upload(command[7:])
-    elif(command.startswith("Download ")):
-        Upload(command[9:])
+    elif command.startswith("Download "):
+        Download(command[9:])
+    elif command.startswith("Delete "):
+        Delete(command[7:])
+    elif command.startswith("Pwd"):
+        Pwd()
     else:
         print("\nCommand not found. type \"Help\" to see commands")
     command = input()
+
+controlSocket.close()
--- FTPserver.py
+++ FTPserver.py
@@ -1,3 +1,4 @@
+import os
 import socket
 from pathlib import Path
 import struct
@@ -17,6 +18,17 @@ dataSocket.bind((IP, dataPort))
 Path("./Files On Server").mkdir(parents=True, exist_ok=True)
 
 
+def List():
+    files = [f for f in os.listdir("./Files On Server")]
+    control_conn.sendall(struct.pack("i", len(files)))
+    control_conn.recv(buffer)
+    for f in files:
+        control_conn.sendall(str(f).encode('utf-8'))
+        control_conn.recv(buffer)
+        size = os.path.getsize("./Files On Server/" + f)
+        control_conn.sendall(struct.pack("i", size))
+        control_conn.recv(buffer)
+    
 
 def Upload():
     dataSocket.listen()
@@ -33,12 +45,51 @@ def Upload():
         file.write(chunck)
         comingSize += buffer
     file.close()
+    data_conn.close()
     dataSocket.close()
+
+
+def Download():
+    dataSocket.listen()
+    data_conn,ad = dataSocket.accept()
+    fileName = control_conn.recv(buffer).decode('utf-8')
+    file_path = "./Files On Server/" + fileName
+    fileSize = os.path.getsize(file_path)
+    control_conn.sendall(struct.pack("i", fileSize))
+    file = open(file_path, "rb")
+    chunk = file.read(buffer)
+    while chunk:
+        data_conn.sendall(chunk)
+        chunk = file.read(buffer)
+    file.close()
+    data_conn.close()
+    dataSocket.close()
+
+
+def Delete():
+    fileName = control_conn.recv(buffer).decode('utf-8')
+    file_path = "./Files On Server/" + fileName
+    os.remove(file_path)
     
+
+def Pwd():
+    control_conn.recv(buffer)
+    path = os.getcwd() + "/Files On Server"
+    control_conn.sendall(path.encode('utf-8'))
+
+
 
 while True:
     command = control_conn.recv(buffer).decode('utf-8')
     control_conn.sendall(str(".").encode('utf-8'))
     match command:
+        case "List":
+            List()
         case "Upload":
             Upload()
+        case "Download":
+            Download()
+        case "Delete":
+            Delete()
+        case "Pwd":
+            Pwd()
