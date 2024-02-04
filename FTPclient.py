ab57a227cfd3fecf6cfc988a8f1136d8f941ca50
setarek1 <101448777+setarek1@users.noreply.github.com>
2024-02-04 10:52:09 +0330

    Update FTPclient.py

--- FTPclient.py
+++ FTPclient.py
@@ -5,12 +5,14 @@ import io
 import struct
 import time
 
+#set up the IP and ports
 IP = "127.0.0.1"
 controlPort = 2121
 dataPort = 2222
 buffer = 1024
+# communication control socket 
 controlSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-
+# the directory where downloaded files will be stored
 Path("./Download Folder").mkdir(parents=True, exist_ok=True)
 
 
@@ -26,10 +28,12 @@ Pwd: Display the current location on the server"""
 
 
 def List():
-    controlSocket.sendall(str("List").encode('utf-8'))
-    controlSocket.recv(buffer)
-    controlSocket.sendall(str(".").encode('utf-8'))
-    num = struct.unpack("i" ,controlSocket.recv(buffer))[0]
+    controlSocket.sendall(str("List").encode('utf-8')) #send List command 
+    controlSocket.recv(buffer)  #recive an acknowledgment from the server
+    controlSocket.sendall(str(".").encode('utf-8'))  #send placeholder character
+    num = struct.unpack("i" ,controlSocket.recv(buffer))[0] #recieve number of files from server
+    if num == 0:
+        print("No Files On Server")
     for i in range(num):
         name = controlSocket.recv(buffer).decode('utf-8')
         controlSocket.sendall(str(".").encode('utf-8'))
@@ -38,35 +42,44 @@ def List():
         # changed_size = size/1000
         # if(size > 1000):
         #     changed_size = changed_size / 1000
-        print(name, '   ', size)
+        print(f"File #{i+1} with Name {name} and Size: {size} bytes")
 
 
 def Connect():
     try:
-        controlSocket.connect((IP,controlPort))
+        controlSocket.connect((IP,controlPort)) #connect to server with IP addr of the server
+                                                #and the port on which the server's listening
+        print("Connected")
     except:
         "unsuccessful connection"
 
 
 def Upload(file_path):
     # time.sleep(1)
-    controlSocket.sendall(str("Upload").encode('utf-8'))
+    controlSocket.sendall(str("Upload").encode('utf-8'))  #send Upload command
     controlSocket.recv(buffer)
     dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     dataSocket.connect((IP, dataPort))
-    fileName = os.path.basename(file_path)
-    controlSocket.sendall(fileName.encode('utf-8'))
+    print("You Are Connected To The Server")
+    fileName = os.path.basename(file_path) #extract file name from path
+    controlSocket.sendall(fileName.encode('utf-8')) #send file name to server
     controlSocket.recv(buffer)
-    fileSize = os.path.getsize(file_path)
-    controlSocket.sendall(struct.pack("i", fileSize))
+    fileSize = os.path.getsize(file_path) #calculate file size
+    controlSocket.sendall(struct.pack("i", fileSize)) #send file size to server
     controlSocket.recv(buffer)
-    file = open(file_path, "rb")
-    chunk = file.read(buffer)
-    while chunk:
-        dataSocket.sendall(chunk)
-        chunk = file.read(buffer)
-    file.close()
-    dataSocket.close()
+    try:
+        file = open(file_path, "rb") #open file in binary mode
+        chunk = file.read(buffer) # read the file in chunk of size given by buffer
+        print("Uploading...")
+        while chunk:
+            dataSocket.sendall(chunk)  #send each chunck
+            chunk = file.read(buffer)
+    except Exception as e:
+        print(f"Error While Uploading File: {e}")
+    finally:
+        print("Upload Is Complete")
+        file.close()
+        dataSocket.close()
 
 
 def Download(fileName):
@@ -74,23 +87,27 @@ def Download(fileName):
     controlSocket.recv(buffer)
     dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     dataSocket.connect((IP, dataPort))
+    print("You Are Connected To The Server")
     controlSocket.sendall(fileName.encode('utf-8'))
     fileSize = struct.unpack("i", controlSocket.recv(buffer))[0]
     file_path = "./Download Folder/" + fileName
-    file = open(file_path, "wb")
+    file = open(file_path, "wb") #create local file in binary write mode
     comingSize = 0
+    print("Downloading...")
     while comingSize < fileSize:
         chunk = dataSocket.recv(buffer)
         file.write(chunk)
         comingSize += buffer
+    print("Download Is Complete")
     file.close()
     dataSocket.close()
 
 
 def Delete(fileName):
-    controlSocket.sendall(str("Delete").encode('utf-8'))
+    controlSocket.sendall(str("Delete").encode('utf-8'))  #send delete command
     controlSocket.recv(buffer)
-    controlSocket.sendall(fileName.encode('utf-8'))
+    controlSocket.sendall(fileName.encode('utf-8')) #indicate which file should be deleted
+    print("Delete Is Complete")
        
 
 def Pwd():
@@ -98,7 +115,7 @@ def Pwd():
     controlSocket.recv(buffer)
     controlSocket.sendall(str(".").encode('utf-8'))
     path = controlSocket.recv(buffer).decode('utf-8')
-    print(path)
+    print(f"Current Directory: {path}")
 
 
 command = input("Client is running. Enter your command (type \"Help\" to see commands)\n")
